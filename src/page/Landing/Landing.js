40f37e3dab3c4ecbab55d74411b0606de5b7fe6e
Daniel Cavazos <daniel_c@comcast.net>
2022-04-29 18:20:00 -0700

    Add sections for 4 main sections of landing page

--- src/page/Landing/Landing.js
+++ src/page/Landing/Landing.js
@@ -4,6 +4,10 @@ function Landing() {
   return(
     <div>
       <h2>Landing Page</h2>
+      <section id="intro"></section>
+      <section id="key_feat"></section>
+      <section id="our_team"></section>
+      <section id="insp"></section>
     </div>
   );
 }
