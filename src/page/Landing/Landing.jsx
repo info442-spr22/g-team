db0e97e38eecf45cbbfa8c9788fcbc27f8bd367e
Daniel Cavazos <daniel_c@comcast.net>
2022-04-29 18:56:26 -0700

    Change Landing.js to Landing.jsx

---
+++ src/page/Landing/Landing.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Landing() {
+  return(
+    <div>
+      <h2>Landing Page</h2>
+      <section className="intro">
+        <div className="intro-txt">
+          <h1>GetWell</h1>
+          <p>Lorem ipsum</p>
+          <div>Button</div>
+        </div>
+        <div className="intro-pic">
+
+        </div>
+      </section>
+      <section className="key_feat"></section>
+      <section className="our_team"></section>
+      <section className="insp"></section>
+    </div>
+  );
+}
+
+export default Landing;
