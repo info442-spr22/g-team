a517c0ea43d646a92285ce36e3c64fa280f0617f
Daniel Cavazos <daniel_c@comcast.net>
2022-04-29 18:15:53 -0700

    Add import and element routing for landing page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import * as urls from './resources/constants/url-endpoints'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css';
+import Landing from './page/Landing/Landing';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path={urls.LANDING} element={<h2>Landing Page</h2>} />
+          <Route path={urls.LANDING} element={<Landing />} />
           <Route path={urls.SCRAPBOOK} element={<h2>Scrapbook Page</h2>} />
         </Routes>
       </BrowserRouter>
