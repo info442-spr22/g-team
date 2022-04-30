43d0ad39bcd8b0b63ee188cee627c611b7883449
fretws <fretws@uw.edu>
2022-04-29 19:48:00 -0700

    End peer coding 4/29

--- src/page/Landing/Landing.jsx
+++ src/page/Landing/Landing.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Landing.module.css';
+import landing_splash from '../../resources/images/landing_splash.svg';
 
 function Landing() {
   return(
@@ -9,15 +10,21 @@ function Landing() {
         <div className={styles.intro_txt}>
           <h1>GetWell</h1>
           <p>Lorem ipsum</p>
-          <div>Button</div>
+          <div>Get Started</div>
         </div>
         <div className={styles.intro_pic}>
-
+          <img src={landing_splash} alt={"Person painting with space"} />
         </div>
       </section>
-      <section className={styles.key_feat}></section>
-      <section className={styles.our_team}></section>
-      <section className={styles.insp}></section>
+      <section className={styles.key_feat}>
+        <h2>Key Features</h2>
+      </section>
+      <section className={styles.our_team}>
+        <h2>Our Team</h2>
+      </section>
+      <section className={styles.insp}>
+        <h3>Inspiration</h3>
+      </section>
     </div>
   );
 }
