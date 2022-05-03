b69de823cd565b8e0599169771fcaa609a8f9fe7
fretws <fretws@uw.edu>
2022-05-03 00:53:34 -0700

    Style nav bar

--- src/component/page-element/NavBar/NavBar.jsx
+++ src/component/page-element/NavBar/NavBar.jsx
@@ -18,8 +18,14 @@ export default function NavBar(props) {
     links.push({ to: urls.PROFILE, text: 'Profile' })
   }
 
+  const activeClass = styles.link + ' ' + styles.active
+
   let linkElements = links.map((link) =>
-    <NavLink to={'/' + link} key={link.id}>
+    <NavLink
+      to={'/' + link.to}
+      className={({ isActive }) => isActive ? activeClass : styles.link}
+      key={link.id}
+    >
       {link.text}
     </NavLink>
   )
@@ -29,9 +35,14 @@ export default function NavBar(props) {
       <h1 className={styles.title}>GetWell</h1>
       {linkElements}
       {!props.authenticated &&
-        <button>
-          <Link to={'/' + urls.AUTH} />
-        </button>
+        <>
+          <button className={styles.button + ' ' + styles.variant}>
+            <Link to={'/' + urls.AUTH}>Log In</Link>
+          </button>
+          <button className={styles.button}>
+            <Link to={'/' + urls.AUTH}>Sign Up</Link>
+          </button>
+        </>
       }
     </div>
   )
