c07fa202cb3b406f7f1f09e9899695c22967503e
Daniel Cavazos <daniel_c@comcast.net>
2022-05-04 18:41:45 -0700

    Remove shortcut functionality to be used in the scrapbook .jsx file later

--- src/component/object-tool-bar/ObjectToolBarStickers.jsx
+++ src/component/object-tool-bar/ObjectToolBarStickers.jsx
@@ -1,36 +1,14 @@
 import React from 'react';
 import styles from './ObjectToolBar.module.css';
-import { useCallback, useEffect } from 'react';
 
 function ObjectToolBarStickers(props) {
 
   // select functionality
   function select(event) {
-    if (event.type === "click") {
-      console.log("Click select functionality TBD");
-    } else if (event.type === "keydown" && event.key === "c") {
-      console.log("Circle shortcut select functionality TBD");
-    }
+    console.log("Click select functionality TBD");
   }
 
-  // Looks for any keypress and runs select if it sees one is pressed down
-  // https://devtrium.com/posts/how-keyboard-shortcut
-  // modified handleKeyPress
-  const handleKeyPress = useCallback((event) => {
-    select(event);
-  }, []);
-
-  useEffect(() => {
-    // attach the event listener
-    document.addEventListener('keydown', handleKeyPress);
-
-    // remove the event listener
-    return () => {
-      document.removeEventListener('keydown', handleKeyPress);
-    };
-  }, [handleKeyPress]);
-
-  function Circle() {
+  function Circle(props) {
     return(
       <svg height = "30" width = "30" className={styles.sticker} onClick={select}>
         <circle cx="15" cy="15" r="15"/>
