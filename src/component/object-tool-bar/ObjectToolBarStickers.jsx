5350821a9e037265f1b8c2ca025799ccfb82cada
Daniel Cavazos <daniel_c@comcast.net>
2022-05-04 17:42:28 -0700

    Change component to allow returning different shapes based on props.type

--- src/component/object-tool-bar/ObjectToolBarStickers.jsx
+++ src/component/object-tool-bar/ObjectToolBarStickers.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import styles from '../Sticker.module.css';
+import styles from './ObjectToolBar.module.css';
 
-function Circle() {
+function ObjectToolBarStickers(props) {
 
   function select() {
     console.log("select functionality TBD");
   }
 
-  return(
-    <svg height = "30" width = "30" className={styles.sticker} onClick={select}>
-      <circle cx="15" cy="15" r="15"/>
-    </svg>
-  );
+  function Circle() {
+    return(
+      <svg height = "30" width = "30" className={styles.sticker} onClick={select}>
+        <circle cx="15" cy="15" r="15"/>
+      </svg>
+    );
+  }
+
+  if (props.type === "circle") {
+    return <Circle />;
+  } else {
+    return <h2>Error Message TBD</h2>;
+  }
 }
 
-export default Circle;
+export default ObjectToolBarStickers;
