649f025b41e7a25f183f9e56a8fab22094d721c0
Niha Gaddam <niha.gaddam@gmail.com>
2022-05-08 15:41:20 -0700

    finished up issue #75

--- src/component/page-element/ActionBar/ActionBar.jsx
+++ src/component/page-element/ActionBar/ActionBar.jsx
@@ -1,16 +1,14 @@
 import React, {useState} from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import {CursorFill} from 'react-bootstrap-icons';
 import styles from "./ActionBar.module.css"
 
 
-export default function ActionBar(props) {
-  const [selected, setSelected] = useState(false);
+export default function ActionBar() {
   return(
     <div className={styles.wrapper}>
     <div className={styles.actionBar}>
       <div className={styles.selectMoveTool}>
-        <button id="selectMove" type="button" class="btn" Click={() => setSelected(true)}>
+        <button id="selectMove" type="button" class="btn selectMove">
           <CursorFill />
         </button>
         </div>
@@ -18,3 +16,8 @@ export default function ActionBar(props) {
   </div>
   );
 }
+
+let button = document.getElementById('selectMove');
+
+button.addEventListener('click', () => button.style.backgroundColor = '#DAF7A2')
+button.addEventListener('click', () => button.style.borderColor = '#1E2F4D')
