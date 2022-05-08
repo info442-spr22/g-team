8a240d5a618714b0bd4bc15c13f84833df69af6a
KexinPei <40810609+KexinPei@users.noreply.github.com>
2022-05-08 16:39:54 -0700

    mouse tracking done but have problem installing the library that need to be used

--- src/page/Scrapbook/Scrapbook.js
+++ src/page/Scrapbook/Scrapbook.js
@@ -2,6 +2,13 @@ import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 
 
+
+// allow users to draw line element for now, use as testing
+function createElement(x1, y1, x2, y2) {
+
+    return {x1, y1, x2, y2};
+}
+
 const Scrapbook = () => {
     const [elements, setElements] = useState([]);
     const [drawing, setDrawing] = useState(false);
@@ -9,20 +16,33 @@ const Scrapbook = () => {
     useLayoutEffect(() => {
         const canvas = document.getElementById("canvas");
         const ctx = canvas.getContext('2d');
+
+        // Re-rendering everytime clicks
         ctx.clearReact(0, 0, canvas.width, canvas.height);
 
+    }, [elements]);
 
-    });
-
+    // mouse tracking
     const handleMouseDown = (event) => {
         setDrawing(true);
+
+        // Starting pt is clientX, clintY and first create element end pt is same as start pt
+        const { clientX, clientY} = event;
+        const element = createElement(clientX, clientY, clientX, clientY);
+        setElements(prevState => [...prevState, element]);
     };
 
     const handleMouseMove = (event) => {
         if(!drawing) return;
 
         const { clientX, clientY} = event;
-        console.log(clientX,clientY);
+        const index = elements.length - 1;
+        const { x1, y1 } = elements[index];
+        const updatedElement = createElement(x1, y1, clientX, clientY);
+
+        const elementsCopy = [...elements];
+        elementsCopy[index] = updatedElement;
+        setElements(elementsCopy);
     };
 
     const handleMouseUp = () => {
@@ -40,3 +60,6 @@ const Scrapbook = () => {
 };
 
 export default Scrapbook;
+
+
+
