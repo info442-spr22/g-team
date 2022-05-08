f7c37df1dcdc789e946f9d852fe3aad0b7ecf776
fretws <fretws@uw.edu>
2022-05-08 01:24:07 -0700

    Add line icon

--- src/component/sticker/tool-bar/ToolBarSticker.jsx
+++ src/component/sticker/tool-bar/ToolBarSticker.jsx
@@ -16,8 +16,26 @@ function ToolBarSticker(props) {
     );
   }
 
+  function Line() {
+    return (
+      <svg
+        width="32" height="32" className={styles.sticker} onClick={select}
+        strokeLinecap="round" strokeWidth="4"
+      >
+        <path
+          fill="none"
+          stroke="black"
+          d="
+            M 2 30
+            L 30 2
+          " />
+      </svg>
+    )
+  }
+
   switch (props.type) {
     case ('circle'): return <Circle />
+    case ('line'): return <Line />
     default:
       return <h2>Error Message TBD</h2>;
   }
