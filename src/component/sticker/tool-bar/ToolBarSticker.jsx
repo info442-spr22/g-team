d63a48914a856c82eb5982fdf329abd526815e9e
Daniel Cavazos <daniel_c@comcast.net>
2022-05-07 22:43:45 -0700

    Add square icon

--- src/component/sticker/tool-bar/ToolBarSticker.jsx
+++ src/component/sticker/tool-bar/ToolBarSticker.jsx
@@ -16,8 +16,17 @@ function ToolBarSticker(props) {
     );
   }
 
+  function Square() {
+    return(
+      <svg height = "30" width = "30" className={styles.sticker} onClick={select}>
+        <rect width="30" height="30"/>
+      </svg>
+    );
+  }
+
   switch (props.type) {
-    case ('circle'): return <Circle />
+    case ('circle'): return <Circle />;
+    case ('square'): return <Square />;
     default:
       return <h2>Error Message TBD</h2>;
   }
