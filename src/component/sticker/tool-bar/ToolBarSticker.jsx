f54a0c5b6693bee2ea0d2119dda11fba1656560b
fretws <fretws@uw.edu>
2022-05-07 22:31:00 -0700

    Add arrow icon

--- src/component/sticker/tool-bar/ToolBarSticker.jsx
+++ src/component/sticker/tool-bar/ToolBarSticker.jsx
@@ -16,8 +16,25 @@ function ToolBarSticker(props) {
     );
   }
 
+  function Arrow() {
+    return (
+      <svg width="32" height="32" className={styles.sticker} onClick={select}>
+        <path fill="none" stroke="black" d="
+          M 2 30
+          L 30 2
+          " />
+        <path fill="none" stroke="black" d="
+          M 20 2
+          H 30
+          V 12
+          " />
+      </svg>
+    )
+  }
+
   switch (props.type) {
     case ('circle'): return <Circle />
+    case ('arrow'): return <Arrow />
     default:
       return <h2>Error Message TBD</h2>;
   }
