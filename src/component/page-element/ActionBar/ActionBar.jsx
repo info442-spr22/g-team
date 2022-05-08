7c417349c4a45bd0ecbef6750c89d409801e9b5d
Niha Gaddam <niha.gaddam@gmail.com>
2022-05-07 18:10:51 -0700

    actionbar changes

--- src/component/page-element/ActionBar/ActionBar.jsx
+++ src/component/page-element/ActionBar/ActionBar.jsx
@@ -3,9 +3,11 @@ import styles from "./ActionBar.module.css"
 
 export default function ActionBar() {
   return(
+    <div className={styles.wrapper}>
     <div className={styles.actionBar}>
 
     </div>
+  </div>
 
   );
 }
