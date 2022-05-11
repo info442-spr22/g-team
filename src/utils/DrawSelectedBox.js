772288435a76beae52f13f8f5baecd95cd8d0d23
cavazd <59621004+cavazd@users.noreply.github.com>
2022-05-11 14:17:52 -0700

    Add line selection

--- src/utils/DrawSelectedBox.js
+++ src/utils/DrawSelectedBox.js
@@ -1,13 +1,28 @@
 import rough from "roughjs/bundled/rough.esm";
 
 export default function drawSelectedBox(element, setSelectedBox, generator) {
+  const type = element.roughElement.shape
   const {x1, x2, y1, y2} = element
+  const minX = Math.min(x1, x2);
+  const minY = Math.min(y1, y2);
   const canvas = document.getElementById("canvas")
   const roughCanvas = rough.canvas(canvas)
-  let selectorBox = generator.rectangle(
-      x1 - 8, y1 - 8, Math.abs(x1 - x2) + 16, Math.abs(y1 - y2) + 16,
-      {strokeLineDash: [10, 10]}
-  )
+  let selectorBox;
+  switch(type) {
+    case("rectangle"):
+        selectorBox = generator.rectangle(
+          minX - 8, minY - 8, Math.abs(x1 - x2) + 16, Math.abs(y1 - y2) + 16,
+          {strokeLineDash: [10, 10]}
+        )
+        break
+    case("line"):
+        selectorBox = generator.rectangle(
+          minX - 8, minY - 8, Math.abs(x1 - x2) + 16, Math.abs(y1 - y2) + 16,
+          {strokeLineDash: [10, 10]}
+        )
+        break
+    default:
+  }
   roughCanvas.draw(selectorBox)
   setSelectedBox(selectorBox)
 }
