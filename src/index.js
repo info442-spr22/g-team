ccf418ade40ab0a0000d5fbf5d19a4046aaacd6c
fretws <fretws@uw.edu>
2022-05-15 13:49:12 -0700

    Make our styles override bootstrap, not the other way around

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import './theme/colors.css'
 import './theme/fonts.css'
 import './theme/sizes.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
