091c626235eec5cfa0682917c00e51c551317109
KexinPei <40810609+KexinPei@users.noreply.github.com>
2022-05-14 17:27:59 -0700

    firebase auth set up but error message due to unable to install package dependency conflict

--- src/App.js
+++ src/App.js
@@ -4,17 +4,80 @@ import './App.css';
 import Landing from './page/Landing/Landing';
 import Scrapbook from './page/Scrapbook/Scrapbook';
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path={urls.LANDING} element={<Landing />} />
-          <Route path={urls.SCRAPBOOK} element={<Scrapbook/>} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+import firebase from 'firebase/app';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+const uiConfig = {
+    //which sign in prociders to use
+    signInOptions: [
+        {
+            provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            // where to show the "display name" on the sign in page
+            requireDisplayName: true
+        }, //each object is a sign in method
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID_ID // sign in with Google
+    ],
+    //page won't show the account chooser
+    credentialHelper: 'none',
+    // use popup instead of redirect for externam sign up methods
+    signInFlow: 'popup'
+};
+
+function App(props) {
+    const [errorMessage, setErrorMessage] = useState(undefined);
+    const [user, setUser] = useState(undefined);
+
+    //register new user
+    const handleSignUp = (email, password, handle, avatar) => {
+        setErrorMessage(null);
+    }
+
+    // log in existing user
+    const handleSignIn = (email, password) => {
+        setErrorMessage(null);
+    }
+
+    //log out current user
+    const handleSignOut = () => {
+        setErrorMessage(null);
+    }
+
+
+    let content = null;
+    
+    if(!user) { //if logged out, show ign up form
+        content = (
+        <div className="container">
+            <h1>Sign Up</h1>
+            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+        </div>
+        );
+    } 
+    else { // if logged in, show welcome message
+        content = (
+            <div>
+                {user &&
+                    <button className="btn btn-warning" onClick={handleSignOut}>
+                        Log Out {user.displayName}
+                    </button>
+                }
+
+                <Switch>
+                    <Routes>
+                    <Route path={urls.LANDING} element={<Landing />} />
+                    <Route path={urls.SCRAPBOOK} element={<Scrapbook/>} />
+                    </Routes>
+
+                </Switch>
+
+            </div>
+        );
+    }
+
+    return (
+        content
+    );
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -6,8 +6,21 @@ import './theme/fonts.css'
 import './theme/sizes.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './custom.css';
+import firebase from 'firebase';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1oBDDtW8s4cClwcedg0VRcd0KmEalYUQ",
+  authDomain: "getwell-info442-gteam.firebaseapp.com",
+  projectId: "getwell-info442-gteam",
+  storageBucket: "getwell-info442-gteam.appspot.com",
+  messagingSenderId: "1035607993662",
+  appId: "1:1035607993662:web:1092a9ecb8500ff0f0d06f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
