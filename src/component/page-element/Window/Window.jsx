087d172e7bf6ce3e19c7ee670b39470c0a0452ca
Niha Gaddam <niha.gaddam@gmail.com>
2022-05-16 10:21:06 -0700

    deleted comments

--- src/component/page-element/Window/Window.jsx
+++ src/component/page-element/Window/Window.jsx
@@ -2,51 +2,10 @@ import React from 'react'
  
 
 export default function Window(props) {
-    // const openWindowButtons = document.querySelectorAll('[data-window-target]');
-    // const closeWindowButtons = document.querySelectorAll('[data-close-button]');
-    // const overlay = document.getElementById('overlay');
 
-    // openWindowButtons.forEach(button => {
-    //     button.addEventListener('click', () => {
-    //         const window = document.querySelector(button.dataset.windowTarget)
-    //         openWindow(window);
-    //     })
-    // });
-    //
-    // function openWindow(window) {
-    //   if(window == null) return
-    //   window.classList.add('active')
-    //   overlay.classList.add('active')
-    // };
-    //
-    // closeWindowButtons.forEach(button => {
-    //   button.addEventListener('click', () => {
-    //       const window = button.closest('.window')
-    //       closeWindow(window);
-    //   })
-    // });
-    //
-    // function closeWindow(window) {
-    //   if(window == null) return
-    //   window.classList.remove('active')
-    //   overlay.classList.remove('active')
-    // };
-    //
-    //
-    // overlay.addEventListener('click', () => {
-    //   const windows = document.querySelectorAll('.window.active');
-    //   windows.forEach(window => {
-    //       closeWindow(window)
-    //   })
-    // });
   
 return(
     <div>
-    {/*<div>*/}
-    {/*<button data-window-target="#window" id="shareButton" type="button" class="btn share">*/}
-    {/*    Share*/}
-    {/*</button>*/}
-    {/*</div>*/}
     <div class="window active" id="window">
     <div class="window-header">
     <div class="title">
