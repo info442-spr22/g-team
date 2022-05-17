1bc7163d14fe5ddcd87e8b6526fc0cea7578117b
Niha Gaddam <niha.gaddam@gmail.com>
2022-05-17 11:32:08 -0700

    fixed positioning of color picker

--- src/component/tool/ColorPicker/ColorPicker.jsx
+++ src/component/tool/ColorPicker/ColorPicker.jsx
@@ -1,7 +1,6 @@
 import React, {useState} from 'react'
 import { SketchPicker } from 'react-color'
 import styles from "../../page-element/ActionBar/ActionBar.module.css"
-import PropertiesSidebar from '../../page-element/PropertiesSidebar/PropertiesSidebar'
 
 
 
@@ -12,19 +11,24 @@ export default function ColorPicker(props) {
   const [open, setOpen] = useState(false);
 
   const canvas = document.getElementById("canvas");
-
-  const openColorPicker = () => {
-    setOpen(!open);
-  }
+  
+  const pickerStyles = {
+    default: {
+      picker: {
+        position: "absolute" ,
+        bottom: "50px"
+      }
+    }
+  };
 
   return(
-    <div>    
-      <div className={"colorWindow " + styles.colorWindow} position="absolute" id="colorWindow">
-      {open &&<SketchPicker color={color} onChangeComplete={(color) => {setColor(color.hex)}} />
-      }
+    <div>   
+      <div className="container-color">
+      {open &&<SketchPicker styles={pickerStyles} color={color} onChangeComplete={(color) => {setColor(color.hex)}} />}
+      
       </div>
       <div>
-      <button onClick={() => openColorPicker()} id="colorPicker" type="button" className={"btn colorPicker " + styles.actionBarButton}>    
+            <button onClick={() => setOpen(open => !open)} id="colorPicker" type="button" className={"btn colorPicker " + styles.actionBarButton}>    
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eyedropper" viewBox="0 0 16 16">
             <path d="M13.354.646a1.207 1.207 0 0 0-1.708 0L8.5 3.793l-.646-.647a.5.5 0 1 0-.708.708L8.293 5l-7.147 7.146A.5.5 0 0 0 1 12.5v1.793l-.854.853a.5.5 0 1 0 .708.707L1.707 15H3.5a.5.5 0 0 0 .354-.146L11 7.707l1.146 1.147a.5.5 0 0 0 .708-.708l-.647-.646 3.147-3.146a1.207 1.207 0 0 0 0-1.708l-2-2zM2 12.707l7-7L10.293 7l-7 7H2v-1.293z"/>
             </svg>
@@ -33,26 +37,13 @@ export default function ColorPicker(props) {
     <div style={{
       backgroundColor: color
     }}>
+
     
     </div>
     </div>
   )
 }
 
-
-// export default function App() {
-//     const [state, updateState] = useState("#FFFFFF");
-  
-//     const handleInput = e => {
-//       updateState(e.target.value);
-//     };
-  
-//     return (
-//       <div className="App">
-//         <ColorPicker onChange={handleInput} value={state} />
-//       </div>
-//     );
-//  }
 
 
 
