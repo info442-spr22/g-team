08cf157bc423cf0bd566484dd19d91bba51b4fe6
Niha Gaddam <niha.gaddam@gmail.com>
2022-05-17 01:05:03 -0700

    added colorpicker

--- src/component/page-element/ActionBar/ActionBar.jsx
+++ src/component/page-element/ActionBar/ActionBar.jsx
@@ -1,8 +1,9 @@
-import React from "react"
+import React, { useState } from "react"
 import {CursorFill, TextareaT} from 'react-bootstrap-icons'
 import {stickerHotKeys} from '../../../page/Scrapbook/Scrapbook'
 import styles from "./ActionBar.module.css"
 import ToolBarSticker from '../../sticker/tool-bar/ToolBarSticker'
+import ColorPicker from "../../tool/ColorPicker/ColorPicker"
 
 
 export default function ActionBar(props) {
@@ -21,7 +22,6 @@ export default function ActionBar(props) {
  
 
 
-
   return(
     <div className={styles.wrapper}>
       <div className={styles.actionBar}>
@@ -29,11 +29,9 @@ export default function ActionBar(props) {
           <button id="selectMove" type="button" className={"btn selectMove " + styles.actionBarButton}>
             <CursorFill />
           </button>
-          <button id="colorPicker" type="button" className="btn colorPicker">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eyedropper" viewBox="0 0 16 16">
-          <path d="M13.354.646a1.207 1.207 0 0 0-1.708 0L8.5 3.793l-.646-.647a.5.5 0 1 0-.708.708L8.293 5l-7.147 7.146A.5.5 0 0 0 1 12.5v1.793l-.854.853a.5.5 0 1 0 .708.707L1.707 15H3.5a.5.5 0 0 0 .354-.146L11 7.707l1.146 1.147a.5.5 0 0 0 .708-.708l-.647-.646 3.147-3.146a1.207 1.207 0 0 0 0-1.708l-2-2zM2 12.707l7-7L10.293 7l-7 7H2v-1.293z"/>
-          </svg>
-          </button>
+        </div>
+        <div>
+        <ColorPicker />
         </div>
           <div onClick={() => {props.setSelectedSticker("text")}}>
               <button id="textBox" type="button" className="btn">
--- src/component/tool/ColorPicker/ColorPicker.jsx
+++ src/component/tool/ColorPicker/ColorPicker.jsx
@@ -1,36 +1,44 @@
-import React from 'react'
-import styled from "styled-components"
+import React, {useState} from 'react'
+import { SketchPicker } from 'react-color'
+import styles from "../../page-element/ActionBar/ActionBar.module.css"
+import PropertiesSidebar from '../../page-element/PropertiesSidebar/PropertiesSidebar'
 
-const Container = styled.span`
 
-  input[type="color"] {
-    margin-right: 8px;
-    -webkit-appearance: none;
-    border: none;
-    width: auto;
-    height: auto;
-    cursor: pointer;
-    background: none;
-    &::-webkit-color-swatch-wrapper {
-      padding: 0;
-      width: 14px;
-      height: 14px;
-    }
-    &::-webkit-color-swatch {
-      border: 1px solid #bfc9d9;
-      border-radius: 4px;
-      padding: 0;
-    }
+
+
+
+export default function ColorPicker(props) {
+  const [color, setColor] = useState('#ff0000');
+  const [open, setOpen] = useState(false);
+
+  const canvas = document.getElementById("canvas");
+
+  const openColorPicker = () => {
+    setOpen(!open);
   }
-`;
 
-const ColorPicker = ({value, onChange, ...rest }) => {
-    return (
-        <Container >
-            <input type="color" onChange={onchange} value={value} {...rest} />
-        </Container>
-    );
-};
+  return(
+    <div>    
+      <div className={"colorWindow " + styles.colorWindow} position="absolute" id="colorWindow">
+      {open &&<SketchPicker color={color} onChangeComplete={(color) => {setColor(color.hex)}} />
+      }
+      </div>
+      <div>
+      <button onClick={() => openColorPicker()} id="colorPicker" type="button" className={"btn colorPicker " + styles.actionBarButton}>    
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eyedropper" viewBox="0 0 16 16">
+            <path d="M13.354.646a1.207 1.207 0 0 0-1.708 0L8.5 3.793l-.646-.647a.5.5 0 1 0-.708.708L8.293 5l-7.147 7.146A.5.5 0 0 0 1 12.5v1.793l-.854.853a.5.5 0 1 0 .708.707L1.707 15H3.5a.5.5 0 0 0 .354-.146L11 7.707l1.146 1.147a.5.5 0 0 0 .708-.708l-.647-.646 3.147-3.146a1.207 1.207 0 0 0 0-1.708l-2-2zM2 12.707l7-7L10.293 7l-7 7H2v-1.293z"/>
+            </svg>
+      </button>
+      </div>
+    <div style={{
+      backgroundColor: color
+    }}>
+    
+    </div>
+    </div>
+  )
+}
+
 
 // export default function App() {
 //     const [state, updateState] = useState("#FFFFFF");
@@ -44,6 +52,7 @@ const ColorPicker = ({value, onChange, ...rest }) => {
 //         <ColorPicker onChange={handleInput} value={state} />
 //       </div>
 //     );
-//   }
+//  }
 
-export default ColorPicker;
+
+
